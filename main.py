c8ee1b141a76a6ac0110ab6835cea82468ec5eb0
ellieayla <1447600+me@users.noreply.github.com>
2026-01-11 17:14:45 -0500

    Faster distance calculations
    Use a Ball Tree (like k-d tree) to partition search space, reducing number of comparisons. Store data in pandas Dataframe to vectorize processing.

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
-from typing import Any, List, Dict, TypedDict, NamedTuple, Tuple, Optional
+from typing import List, Tuple
 import json
 from pathlib import Path
-from geopy.distance import geodesic, Distance
-import sys
+
+
 from geojson import Feature, Point, FeatureCollection
-from progress.bar import Bar
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import BallTree
+
+
+EARTH_RADIUS_KM = 6371.009
+
+
+def df_from_osm_json_file(json_file: Path) -> pd.DataFrame:
+
+    with open(json_file, 'r') as f:
+        document = json.load(f)
+
+    assert document['generator']
+
+    elements = document['elements']
+
+    assert len(elements) > 0
+
+    # squish center points from Ways down to lat/lon like Points, and discard the center.
+    for elem in elements:
+        if 'center' in elem:
+            elem['lat'] = elem['center']['lat']
+            elem['lon'] = elem['center']['lon']
+            del elem['center']
+            del elem['nodes']
+
+    df = pd.DataFrame(elements)
+
+    for column in ('lat', 'lon'):
+        df[f'{column}_rad'] = np.deg2rad(df[column].values)
+        
+    return df
+
 
 def load_elements_from_json_file(filename: str) -> List:
     with open(filename, 'r') as f:
@@ -15,8 +48,15 @@ def load_elements_from_json_file(filename: str) -> List:
         elements = document['elements']
 
         assert len(elements) > 0
-        return elements
 
+        # squish center points from Ways down to lat/lon like Points, and discard the center.
+        for elem in elements:
+            if 'center' in elem:
+                elem['lat'] = elem['center']['lat']
+                elem['lon'] = elem['center']['lon']
+                del elem['center']
+                del elem['nodes']
+        return elements
 
 
 def get_lat_lon_from_osm_nwr(nwr: dict) -> Tuple[float, float]:
@@ -26,76 +66,71 @@ def get_lat_lon_from_osm_nwr(nwr: dict) -> Tuple[float, float]:
         return nwr['center']['lat'], nwr['center']['lon']
 
 
-def find_closest_in_collection(node_of_interest, collection_of_nodes: List[dict]) -> Tuple[dict[str, Any], Distance]:
-    closest_from_collection: Optional[dict[str, Any]] = None
-    approximate_closest_distance_squared: Optional[float] = None
+def find_closest_in_collection(query_df: pd.DataFrame, bicycle_parking_for_tree: pd.DataFrame) -> pd.DataFrame:
 
-    node_of_interest_lat, node_of_interest_lon = get_lat_lon_from_osm_nwr(node_of_interest)
+    # Ball Tree is a specialized version of a K-D tree that works in radians.
+    search_ball = BallTree(bicycle_parking_for_tree[["lat_rad", "lon_rad"]].values, metric="haversine")
 
-    for n in collection_of_nodes:
-
-        # inputs to geodesic are (lat, long) tuples
-        possible_closest_lat, possible_closest_lon = get_lat_lon_from_osm_nwr(n)
-        
-        # https://en.wikibooks.org/wiki/Algorithms/Distance_approximations
-        # use a very crude approximation of distance,
-        # assuming the earth is a _flat plane_.
-        # this is close enough 
-        # don't actually compute the square root of approximate_distance_squared
-        # sqrt is strictly monotonic & increasing, just compare the inputs to determine which is larger
-        approximate_distance_squared = (
-            (node_of_interest_lat - possible_closest_lat)**2
-            +
-            (node_of_interest_lon - possible_closest_lon)**2
-        )
-
-        if approximate_closest_distance_squared is None or approximate_closest_distance_squared > approximate_distance_squared:
-            approximate_closest_distance_squared = approximate_distance_squared
-            closest_from_collection = n
-
-    if closest_from_collection is None:
-        raise ValueError("Unable to find any distance to item in source collection.")
-
-    # only compute once, for final object
-    closest_distance = geodesic(get_lat_lon_from_osm_nwr(node_of_interest), get_lat_lon_from_osm_nwr(closest_from_collection) )
-    return (closest_from_collection, closest_distance)
-
-
-def create_feature_about_subject(node_of_interest: dict, distance: Distance, closest_item: dict) -> Feature:
-        # Make a Feature about the /node of interest/
-
-    """
-    { "type": "Feature",
-      "geometry": { "type": "Point", "coordinates": [ -151.5129, 63.1016, 0.0 ] }
-      "properties": { "id": "ak16994521", "mag": 2.3, "time": 1507425650893, "felt": null, "tsunami": 0 },
-    },
-    """
-
-    # https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.1
-    # The first two elements are longitude and latitude, ...  precisely in that order and using decimal numbers.
-    # That's reversed from input data.
-    lat, lon = get_lat_lon_from_osm_nwr(node_of_interest)
-    location = Point((lon, lat))
-
-    properties: dict = node_of_interest['tags'].copy()
-    properties['id'] = node_of_interest['id']
-    properties['type'] = node_of_interest['type']
-
-    properties["_distance_meters"] = distance.m
-
-    # helper numeric flags for maplibre styles
-    properties["_under_one_minute"] = 1 if distance.m < 80 else 0  # 1 minute == 80 metres
-    properties["_under_two_minute"] = 1 if distance.m < 160 else 0  # 2 minute == 160 metres
+    # k results to find
+    k = 1
     
-    properties['_closest'] = {
-        "id": closest_item['id'],
-        "type": closest_item['type'],
-    }
+    # vectorized work:
+    #   - for each query, find the closest item in the Ball Tree,
+    #          and emit its distance (unit-sphere) and index in the Ball Tree's original dataset (bicycle_parking_for_tree)
+    distances, indicies = search_ball.query(query_df[["lat_rad", "lon_rad"]].values, k=k)
 
-    return Feature(
-        geometry=location,
-        properties=properties
+    # indicies is a vector of integer indexes with the same length as the query terms above.
+    # Each index identifies a row in the bicycle_parking_for_tree dataframe.
+    # We'll merge the two dataframes on this index later.
+    # Store the vector with the original search query.
+    query_df['index_of_closest'] = indicies
+    
+    # distances is a vector of float values with the same length as the query terms above.
+    # Each distance float was produced assuming the planet is a unit sphere.
+    # Scale each distance to meters, and store the vector with the original search query.
+    query_df[f'distance_meters'] = (lambda d: d * EARTH_RADIUS_KM * 1000)(distances)
+
+    result_df = pd.merge(
+        left=query_df, right=bicycle_parking_for_tree,
+        left_on = "index_of_closest", right_index=True,
+        suffixes=('', '_closest'),
+        validate='many_to_one'
     )
+
+    # drop all columns in radians, no longer needed
+    del result_df['lat_rad']
+    del result_df['lon_rad']
+    del result_df['lat_rad_closest']
+    del result_df['lon_rad_closest']
+
+    # drop extra information about the bike racks
+    del result_df['index_of_closest']
+    del result_df['tags_closest']
+
+    return result_df
+
+
+def df_to_geojson(df: pd.DataFrame, lat='lat', lon='lon') -> FeatureCollection:
+    features = df.apply(
+        # this lambda function accepts a row from the dataframe,
+        # and produces a Feature.
+        lambda row: Feature(
+            # geojson coordinates are reversed (lon/lat) compared to everything else here (lat/lon)
+            geometry=Point((row[lon], row[lat])),
+            properties=dict(row['tags'], **{
+                "id": row["id"],
+                "type": row["type"],
+                "_id_closest": row["id_closest"],
+                "_type_closest": row["type_closest"],
+                "_distance_meters": row["distance_meters"],
+                "_under_one_minute": 1 if row["distance_meters"] < 80 else 0, # 1 minute == 80 metres
+                "_under_two_minute": 1 if row["distance_meters"] < 160 else 0,  # 2 minute == 160 metres
+                })
+        ),
+        axis=1,
+    ).to_list()
+    
+    return FeatureCollection(features=features)
 
 
 def main():
@@ -108,27 +143,23 @@ def main():
     
     a: Namespace = p.parse_args()
 
-    amenities = load_elements_from_json_file(a.amenities)
-    bicycle_parking = load_elements_from_json_file(a.parking)
+    amenities_df = df_from_osm_json_file(a.amenities)
+    search_space_for_tree_df = df_from_osm_json_file(a.parking)
 
-    if len(bicycle_parking) == 0:
+    if len(search_space_for_tree_df) == 0:
         p.error("Parking list is empty, impossible to find closest.")
 
-    print(f"Finding distance from {len(amenities)} features to nearest of {len(bicycle_parking)} parking locations ({len(amenities) * len(bicycle_parking)} deltas)")
-    with Bar(max=len(amenities)) as progress_bar:
-        output_features = []
-        for input_feature in amenities:
-            closest_item, distance = find_closest_in_collection(input_feature, bicycle_parking)
-            annotated_feature_for_heatmap = create_feature_about_subject(input_feature, distance, closest_item)
-            output_features.append(annotated_feature_for_heatmap)
-            progress_bar.next()
-        
-        output_collection = FeatureCollection(output_features)
+    print(f"Finding distance from {len(amenities_df)} features to nearest of {len(search_space_for_tree_df)} parking locations")
 
+    results_df = find_closest_in_collection(amenities_df, search_space_for_tree_df)
+    
+    output_collection = df_to_geojson(results_df)
+    
     with open(a.output, 'w') as f:
         json.dump(output_collection, f, indent=2)
 
-    print(f"Wrote {len(output_features)} features")
+    print(f"Wrote {len(output_collection['features'])} features")
+
 
 if __name__ == "__main__":
     main()
---
+++ test_distance_approximation.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+from geojson import Feature, FeatureCollection, dumps
+from main import df_to_geojson, EARTH_RADIUS_KM
+
+
+def test_dataframe_to_geojson_feature_collection():
+    
+    merge_result_dict = {'type': {0: 'way', 1: 'way', 2: 'way', 3: 'way'}, 'id': {0: 13960135, 1: 24835556, 2: 25338806, 3: 25464956}, 'tags': {0: {'addr:city': 'Burlington', 'addr:housenumber': '777', 'addr:street': 'Guelph Line', 'building': 'retail', 'building:levels': '1', 'name': 'Burlington Centre', 'opening_hours': 'Mo-Fr 10:00-21:00; Sa 09:30-18:00; Su 12:00-17:00', 'shop': 'mall', 'smoking': 'outside', 'wikidata': 'Q4999257', 'wikipedia': 'en:Burlington Centre'}, 1: {'addr:city': 'Burlington', 'addr:housenumber': '867', 'addr:postcode': 'L7S 1A1', 'addr:province': 'ON', 'addr:street': 'Lakeshore Road', 'building': 'yes', 'building:levels': '7', 'government': 'public_service', 'name': 'Canada Centre for Inland Waters', 'office': 'research', 'operator': 'Environment and Climate Change Canada', 'website': 'https://profils-profiles.science.gc.ca/en/research-centre/canada-centre-inland-waters'}, 2: {'addr:housenumber': '2025', 'addr:street': 'Guelph Line', 'building': 'retail', 'building:levels': '1', 'roof:levels': '0', 'toilets': 'yes', 'toilets:wheelchair': 'yes', 'wheelchair': 'yes'}, 3: {'building': 'block', 'name': 'AIC'}}, 'lat': {0: 43.3483465, 1: 43.2989701, 2: 43.3666377, 3: 43.3412267}, 'lon': {0: -79.7935195, 1: -79.8006915, 2: -79.8222113, 3: -79.8339227}, 'distance_meters': {0: 132.85147549600796, 1: 81.44035331775225, 2: 53.52223280841795, 3: 191.8336399157924}, 'type_closest': {0: 'way', 1: 'way', 2: 'node', 3: 'way'}, 'id_closest': {0: 154904418, 1: 442230811, 2: 12663529968, 3: 186353929}, 'lat_closest': {0: 43.3494614, 1: 43.2991818, 2: 43.3663466, 3: 43.3428995}, 'lon_closest': {0: -79.7941101, 1: -79.7997281, 2: -79.821684, 3: -79.8333425}}
+    
+    # results of processing full dataset have this shape
+    merge_result = pd.DataFrame.from_dict(merge_result_dict)
+    print(merge_result)
+
+    # SUT
+    feature_collection: FeatureCollection = df_to_geojson(merge_result)
+
+    # verify
+    assert len(feature_collection['features']) == 4
+
+    for feature in feature_collection['features']:
+        assert isinstance(feature, Feature)
+        assert 10 < feature["properties"]["_distance_meters"] < 200  # everything is nearby
+        assert feature["properties"]['_under_one_minute'] in (0, 1)
+
+        assert -79.9 < feature["geometry"]["coordinates"][0] < -79.4  # source data longitudes are all in this range
+
+    # can be serialized
+    _ = dumps(feature_collection, indent=2)
+
+
+def test_ball_tree_plan():
+    from main import find_closest_in_collection
+
+    query_df = pd.DataFrame.from_dict(
+        {'type': {0: 'way'}, 'id': {0: 13960135}, 'tags': {0: {'addr:city': 'Burlington', 'addr:housenumber': '777', 'addr:street': 'Guelph Line', 'building': 'retail', 'building:levels': '1', 'name': 'Burlington Centre', 'opening_hours': 'Mo-Fr 10:00-21:00; Sa 09:30-18:00; Su 12:00-17:00', 'shop': 'mall', 'smoking': 'outside', 'wikidata': 'Q4999257', 'wikipedia': 'en:Burlington Centre'}}, 'lat': {0: 43.3483465}, 'lon': {0: -79.7935195},}
+    )
+    for column in ('lat', 'lon'):
+        query_df[f'{column}_rad'] = np.deg2rad(query_df[column].values)
+
+    bicycle_parking_for_tree = pd.DataFrame.from_dict(
+        {'type': {0: 'node', 1: 'node', 2: 'way', 3: 'node'}, 'id': {0: 1356846891, 1: 1615155957, 2: 1423550542, 3: 11615021109}, 'lat': {0: 43.3133162, 1: 43.3871208, 2: 43.3133067, 3: 43.3470884}, 'lon': {0: -79.8534052, 1: -79.8116887, 2: -79.8533965, 3: -79.7940899}, 'tags': {0: {'amenity': 'bicycle_parking', 'bicycle_parking': 'rack', 'capacity': '16', 'covered': 'yes', 'operator': 'GO Transit'}, 1: {'amenity': 'bicycle_parking'}, 2: {'access': 'yes', 'amenity': 'bicycle_parking', 'bicycle_parking': 'shed', 'building': 'shed', 'capacity': '16', 'covered': 'yes', 'fee': 'no'}, 3: {'amenity': 'bicycle_parking', 'loc': 'mall_edge'}}, }
+    )
+
+    for column in ('lat', 'lon'):
+        bicycle_parking_for_tree[f'{column}_rad'] = np.deg2rad(bicycle_parking_for_tree[column].values)
+
+    result = find_closest_in_collection(query_df, bicycle_parking_for_tree)
+    print(result)
+
+    assert len(result) == 1
+
+    r: pd.DataFrame = result[['id_closest']]
+    assert r.to_dict() == {'id_closest': {0: 11615021109}}  # a rack right outside the mall
