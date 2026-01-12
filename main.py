4153cc0c0856221ef57c7c2da4deef99901ac596
ellieayla <1447600+me@users.noreply.github.com>
2026-01-12 13:51:35 -0500

    Experiment: determine distance for both bikes and cars and calculate the disparity (in metres)

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def load_elements_from_json_file(filename: str) -> List:
         return elements
 
 
-def find_closest_in_collection(query_df: pd.DataFrame, bicycle_parking_for_tree: pd.DataFrame) -> pd.DataFrame:
+def find_closest_in_collection(query_df: pd.DataFrame, bicycle_parking_for_tree: pd.DataFrame, labeled="_closest") -> pd.DataFrame:
 
     # Ball Tree is a specialized version of a K-D tree that works in radians.
     search_ball = BallTree(bicycle_parking_for_tree[["lat_rad", "lon_rad"]].values, metric="haversine")
@@ -76,29 +76,25 @@ def find_closest_in_collection(query_df: pd.DataFrame, bicycle_parking_for_tree:
     # Each index identifies a row in the bicycle_parking_for_tree dataframe.
     # We'll merge the two dataframes on this index later.
     # Store the vector with the original search query.
-    query_df['index_of_closest'] = indicies
+    query_df['index_of' + labeled] = indicies
     
     # distances is a vector of float values with the same length as the query terms above.
     # Each distance float was produced assuming the planet is a unit sphere.
     # Scale each distance to meters, and store the vector with the original search query.
-    query_df[f'distance_meters'] = (lambda d: d * EARTH_RADIUS_KM * 1000)(distances)
+    query_df[f'distance_meters' + labeled] = (lambda d: d * EARTH_RADIUS_KM * 1000)(distances).round()
 
     result_df = pd.merge(
         left=query_df, right=bicycle_parking_for_tree,
-        left_on = "index_of_closest", right_index=True,
-        suffixes=('', '_closest'),
+        left_on = 'index_of' + labeled, right_index=True,
+        suffixes=('', labeled),
         validate='many_to_one'
     )
 
-    # drop all columns in radians, no longer needed
-    del result_df['lat_rad']
-    del result_df['lon_rad']
-    del result_df['lat_rad_closest']
-    del result_df['lon_rad_closest']
-
-    # drop extra information about the bike racks
-    del result_df['index_of_closest']
-    del result_df['tags_closest']
+    # drop extra information about the right side
+    del result_df['lat_rad' + labeled]
+    del result_df['lon_rad' + labeled]
+    del result_df['index_of' + labeled]
+    del result_df['tags' + labeled]
 
     return result_df
 
@@ -117,9 +113,9 @@ def df_to_geojson(df: pd.DataFrame, lat='lat', lon='lon') -> FeatureCollection:
                 "_type_closest": row["type_closest"],
                 "_lat_closest": row["lat_closest"],
                 "_lon_closest": row["lon_closest"],
-                "_distance_meters": row["distance_meters"],
-                "_under_one_minute": 1 if row["distance_meters"] < 80 else 0, # 1 minute == 80 metres
-                "_under_two_minute": 1 if row["distance_meters"] < 160 else 0,  # 2 minute == 160 metres
+                "_distance_meters": row["distance_meters_closest"],
+                "_under_one_minute": 1 if row["distance_meters_closest"] < 80 else 0, # 1 minute == 80 metres
+                "_under_two_minute": 1 if row["distance_meters_closest"] < 160 else 0,  # 2 minute == 160 metres
                 })
         ),
         axis=1,
@@ -142,9 +138,9 @@ def df_to_geojson_with_catchment_lines(df: pd.DataFrame, lat='lat', lon='lon') -
                 "_type_closest": row["type_closest"],
                 "_lat_closest": row["lat_closest"],
                 "_lon_closest": row["lon_closest"],
-                "_distance_meters": row["distance_meters"],
-                "_under_one_minute": 1 if row["distance_meters"] < 80 else 0, # 1 minute == 80 metres
-                "_under_two_minute": 1 if row["distance_meters"] < 160 else 0,  # 2 minute == 160 metres
+                "_distance_meters": row["distance_meters_closest"],
+                "_under_one_minute": 1 if row["distance_meters_closest"] < 80 else 0, # 1 minute == 80 metres
+                "_under_two_minute": 1 if row["distance_meters_closest"] < 160 else 0,  # 2 minute == 160 metres
                 })
         ),
         axis=1,
@@ -153,16 +149,42 @@ def df_to_geojson_with_catchment_lines(df: pd.DataFrame, lat='lat', lon='lon') -
     return FeatureCollection(features=features)
 
 
+def df_to_geojso_with_disparity(df: pd.DataFrame, disparity_column_suffix: str, lat='lat', lon='lon') -> FeatureCollection:
+    features = df.apply(
+        # this lambda function accepts a row from the dataframe,
+        # and produces a Feature.
+        lambda row: Feature(
+            # geojson coordinates are reversed (lon/lat) compared to everything else here (lat/lon)
+            geometry=Point((row[lon], row[lat])),
+            properties=dict(row['tags'], **{
+                "id": row["id"],
+                "type": row["type"],
+                "_id_closest": row["id_closest"],
+                "_type_closest": row["type_closest"],
+                "_lat_closest": row["lat_closest"],
+                "_lon_closest": row["lon_closest"],
+                "_distance_meters": row["distance_meters_closest"],
+                "_distance_meters_disparity": row["distance_meters" + disparity_column_suffix],
+                "_difference_distance_disparity": row["distance_meters" + disparity_column_suffix] - row["distance_meters_closest"],
+                "_under_one_minute": 1 if row["distance_meters_closest"] < 80 else 0, # 1 minute == 80 metres
+                "_under_two_minute": 1 if row["distance_meters_closest"] < 160 else 0,  # 2 minute == 160 metres
+                })
+        ),
+        axis=1,
+    ).to_list()
+    
+    return FeatureCollection(features=features)
+
 
 def main():
-    from argparse import ArgumentParser, Namespace, FileType
+    from argparse import ArgumentParser, Namespace
 
     p = ArgumentParser(description="For each element in $amenities, find the closest $parking")
     p.add_argument("--amenities", type=Path, metavar="shops.json", required=True)
     p.add_argument("--parking", type=Path, metavar="bicycle-parking.json", required=True)
     p.add_argument("--output", type=Path, metavar="output.geojson", required=True)
     p.add_argument("--catchment-lines", action="store_true")
-    
+    p.add_argument("--disparity-with", type=Path, metavar="car-parking,json")
     a: Namespace = p.parse_args()
 
     amenities_df = df_from_osm_json_file(a.amenities)
@@ -174,8 +196,15 @@ def main():
     print(f"Finding distance from {len(amenities_df)} features to nearest of {len(search_space_for_tree_df)} parking locations")
 
     results_df = find_closest_in_collection(amenities_df, search_space_for_tree_df)
-    
-    if a.catchment_lines:
+
+
+    if a.disparity_with:
+        disparity_df = df_from_osm_json_file(a.disparity_with)
+        print(f"Finding distance from {len(amenities_df)} features to nearest of {len(disparity_df)} disparity locations")
+        difference_df = find_closest_in_collection(results_df, disparity_df, labeled="_disparity")
+        output_collection = df_to_geojso_with_disparity(difference_df, disparity_column_suffix="_disparity")
+
+    elif a.catchment_lines:
         output_collection = df_to_geojson_with_catchment_lines(results_df)
     else:
         output_collection = df_to_geojson(results_df)
