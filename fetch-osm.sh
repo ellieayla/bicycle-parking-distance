438f3e136d15668dd7454278990890a3f6cee9c4
ellieayla <1447600+me@users.noreply.github.com>
2026-03-13 13:12:27 -0400

    Revert "Swap overpass api endpoint"
    This reverts commit e9d3195a42361bbc5bd222cda72ccf7803c015fb.

--- fetch-osm.sh
+++ fetch-osm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # usage: cat overpass-query.ql | ./fetch-osm.sh > osm-data.json
-curl --fail --retry 8 --retry-delay 30 -v -d @- -X POST https://overpass.kumi.systems/api/interpreter
+curl --fail --retry 8 --retry-delay 30 -v -d @- -X POST http://overpass-api.de/api/interpreter
